b8d31ccbf4152482daa2772ee515acea55d80413
ahmadalnujaidi <ahmadalnujaidi02@gmail.com>
2024-11-06 12:25:55 +0300

    deployment phase1

--- app.py
+++ app.py
@@ -137,5 +137,4 @@ def get_detection_log():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
-    app.run(host="0.0.0.0", port=port)
+
