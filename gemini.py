0fedb20e38618d5c2ea23a9623b34f6daec10a8d
ahmadalnujaidi <ahmadalnujaidi02@gmail.com>
2024-11-06 12:22:07 +0300

    removed gemini

--- gemini.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-import os
-
-genai.configure(api_key="AIzaSyCWQVBBgIRIowkV3aBD1UxaNfuqLyiT1q0")
-
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("dsecribe saudi arabia in 5 words.")
-print(response.text)
